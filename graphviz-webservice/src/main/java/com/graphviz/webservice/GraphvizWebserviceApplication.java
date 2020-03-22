9a9adf600f5e4d28c0cc1bc2a7847553344da6b5
Basavaraj K N <Basavaraj.K@Stackroute.in>
2020-03-22 13:23:39 +0530

    initial commit

---
+++ graphviz-webservice/src/main/java/com/graphviz/webservice/GraphvizWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.graphviz.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphvizWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphvizWebserviceApplication.class, args);
+	}
+
+}
